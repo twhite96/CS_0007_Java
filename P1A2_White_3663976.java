6f84e461c8a80e9e849b65f4bde17a325a79c505
Tiffany White <tiffanywhitedev412@gmail.com>
2015-09-08 22:38:46 -0400

    Adds escape sequences to print statements

--- P1A2_White_3663976.java
+++ P1A2_White_3663976.java
@@ -14,18 +14,18 @@ public class P1A2_White_3663976
 	int foodBudget = 235;
 	int entertainment = 100;
 	int carExpenses = 200;
-	System.out.print("hourlyRate = " + hourlyRate);
-	System.out.print("overtime = " + overtime);
-	System.out.print("regularHoursWorked = " + regularHoursWorked);
-	System.out.print("overtimeHoursWorked = " + overtimeHoursWorked);
-	System.out.print("rent = " + rent);
-	System.out.print("elecBill = " + elecBill);
-	System.out.print("waterBill = " + waterBill);
-	System.out.print("sewBill = " + sewBill);
-	System.out.print("gasBill = " + gasBill);
-	System.out.print("foodBudget = " + foodBudget);
-	System.out.print("entertainment =" + entertainment);
-	System.out.print("carExpenses = " + carExpenses);
+	System.out.print("hourlyRate = " + hourlyRate + "\n");
+	System.out.print("overtime = " + overtime + "\n");
+	System.out.print("regularHoursWorked = " + regularHoursWorked + "\n");
+	System.out.print("overtimeHoursWorked = " + overtimeHoursWorked + "\n");
+	System.out.print("rent = " + rent + "\n");
+	System.out.print("elecBill = " + elecBill + "\n");
+	System.out.print("waterBill = " + waterBill + "\n");
+	System.out.print("sewBill = " + sewBill + "\n");
+	System.out.print("gasBill = " + gasBill + "\n");
+	System.out.print("foodBudget = " + foodBudget + "\n");
+	System.out.print("entertainment =" + entertainment + "\n");
+	System.out.print("carExpenses = " + carExpenses + "\n");
 	System.out.println();
 	}
 }
