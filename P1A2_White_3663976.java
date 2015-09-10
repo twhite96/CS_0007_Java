3376d6619e3c0088ff1ee7bdedea7ba6a912c149
Tiffany White <tiffanywhitedev412@gmail.com>
2015-09-10 13:59:43 -0400

    Updated concatenated print statement

--- P1A2_White_3663976.java
+++ P1A2_White_3663976.java
@@ -14,18 +14,16 @@ public class P1A2_White_3663976
 	int foodBudget = 235;
 	int entertainment = 100;
 	int carExpenses = 200;
-	System.out.print("hourlyRate = " + hourlyRate + "\n");
-	System.out.print("overtime = " + overtime + "\n");
-	System.out.print("regularHoursWorked = " + regularHoursWorked + "\n");
-	System.out.print("overtimeHoursWorked = " + overtimeHoursWorked + "\n");
-	System.out.print("rent = " + rent + "\n");
-	System.out.print("elecBill = " + elecBill + "\n");
-	System.out.print("waterBill = " + waterBill + "\n");
-	System.out.print("sewBill = " + sewBill + "\n");
-	System.out.print("gasBill = " + gasBill + "\n");
-	System.out.print("foodBudget = " + foodBudget + "\n");
-	System.out.print("entertainment =" + entertainment + "\n");
-	System.out.print("carExpenses = " + carExpenses + "\n");
-	System.out.println();
+	System.out.println("hourlyRate = " + hourlyRate + "\n" +
+		"overtime = " + overtime + "\n" + "regularHoursWorked = " + regularHoursWorked + "\n" +
+		"overtimeHoursWorked = " + overtimeHoursWorked + "\n" +
+		"rent = " + rent + "\n" +
+		"elecBill = " + elecBill + "\n" + 
+		"waterBill = " + waterBill + "\n" +
+		"sewBill = " + sewBill + "\n" + 
+		"gasBill = " + gasBill + "\n" + 
+		"foodBudget = " + foodBudget + "\n" + 
+		"entertainment =" + entertainment + "\n" + 
+		"carExpenses = " + carExpenses + "\n");
 	}
 }
