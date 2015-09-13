5c6de8abae1ea206553803be601845ec12ada9e3
Tiffany White <tiffanywhitedev412@gmail.com>
2015-09-13 17:52:40 -0400

    Updated variable initalizations

--- P1A2_White_3663976.java
+++ P1A2_White_3663976.java
@@ -2,28 +2,29 @@ public class P1A2_White_3663976
 {
 	public static void main(String[] args)
 	{
-	int hourlyRate = 30;
+	int hourlyRate, regularHoursWorked, overtimeHoursWorked, rent, elecBill, waterBill, sewBill, gasBill, foodBudget, entertainment, carExpenses;
 	double overtime = 30.5;
-	int regularHoursWorked = 80;
-	int overtimeHoursWorked = 10;
-	int rent = 800;
-	int elecBill = 10;
-	int waterBill = 15;
-	int sewBill = 6;
-	int gasBill = 10;
-	int foodBudget = 235;
-	int entertainment = 100;
-	int carExpenses = 200;
+	hourlyRate = 30;
+	regularHoursWorked = 80;
+	overtimeHoursWorked = 10;
+	rent = 800;
+	elecBill = 10;
+	waterBill = 15;
+	sewBill = 6;
+	gasBill = 10;
+	foodBudget = 235;
+	entertainment = 100;
+	carExpenses = 200;
 	System.out.println("hourlyRate = " + hourlyRate + "\n" +
 		"overtime = " + overtime + "\n" + "regularHoursWorked = " + regularHoursWorked + "\n" +
 		"overtimeHoursWorked = " + overtimeHoursWorked + "\n" +
 		"rent = " + rent + "\n" +
-		"elecBill = " + elecBill + "\n" + 
+		"elecBill = " + elecBill + "\n" +
 		"waterBill = " + waterBill + "\n" +
-		"sewBill = " + sewBill + "\n" + 
-		"gasBill = " + gasBill + "\n" + 
-		"foodBudget = " + foodBudget + "\n" + 
-		"entertainment =" + entertainment + "\n" + 
+		"sewBill = " + sewBill + "\n" +
+		"gasBill = " + gasBill + "\n" +
+		"foodBudget = " + foodBudget + "\n" +
+		"entertainment =" + entertainment + "\n" +
 		"carExpenses = " + carExpenses + "\n");
 	}
 }
