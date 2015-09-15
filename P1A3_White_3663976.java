1c8f4e45c07dc387ffd882fe9b38af7be6c5c51b
Tiffany White <tiffanywhitedev412@gmail.com>
2015-09-15 11:42:09 -0400

    Adds System out statement to print name variable to console

--- P1A3_White_3663976.java
+++ P1A3_White_3663976.java
@@ -1,6 +1,7 @@
 /* Author name: Tiffany White
 This is a budget calculator
 Created date: September 15, 2015
+Last modified date: September 15, 2015
 */
 
 import java.util.Scanner;
@@ -12,5 +13,7 @@ public class P1A3_White_3663976
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("What is your name?");
 		String name = keyboard.nextLine();
+		System.out.print("Welcome to Budget Calculator by Tiffany White, " + name +
+		". \nIt will calculate your income and expenses.");
 	}
 }
