f30142fd8338dcab1496132d19f6a203835116f4
Tiffany White <tiffanywhitedev412@gmail.com>
2015-09-15 11:23:37 -0400

    Updates activity 3 with Scanner class and methods

--- P1A3_White_3663976.java
+++ P1A3_White_3663976.java
@@ -1,7 +1,11 @@
-public class
+import java.util.Scanner;
+
+public class P1A3_White_3663976
 {
 	public static void main(String[] args)
 	{
-	System.out.println()
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("What is your name?");
+		String name = keyboard.nextLine();
 	}
 }
