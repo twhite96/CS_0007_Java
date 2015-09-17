0fd5e63a4bd01f7c929464a929d0d0c8152ce265
Tiffany White <tiffanywhitedev412@gmail.com>
2015-09-17 15:04:44 -0400

    Adds class participation java file

---
+++ White.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class White
+{
+	public static void main(String[] args)
+	{
+  Scanner keyboard = new Scanner(System.in);
+
+	System.out.print("Enter your first test score.");
+
+  double firstScore = keyboard.nextDouble();
+
+	System.out.print("Enter your second test score.");
+
+	double secondScore = keyboard.nextDouble();
+
+	System.out.print("Enter your third test score.");
+
+	double thirdScore = keyboard.nextDouble();
+
+	double totalScore = firstScore + secondScore + thirdScore / 3;
+
+	System.out.printf("Your class average is %,.2f%\n", totalScore);
+	}
+}
